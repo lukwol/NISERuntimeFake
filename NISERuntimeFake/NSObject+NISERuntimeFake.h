ddb3bd08b0839279c5ce25678bdcd0cbca8f4c24
Lukasz Wolanczyk <wolanczyk.lukasz@gmail.com>
2014-03-05 22:50:21 +0100

    Code refactor

--- NISERuntimeFake/NSObject+NISERuntimeFake.h
+++ NISERuntimeFake/NSObject+NISERuntimeFake.h
@@ -16,16 +16,16 @@
 
 /**
  Creates not registered subclass of any class inheriting from NSObject and creates fake object of this class with default initializer.
- 
+
  @return A new fake object.
 */
 + (id)fake;
 
 /**
  Creates not registered subclass of any class inheriting from NSObject implementing all methods from selected protocol and creates fake object of this class with default initializer.
- 
+
  @param protocol The protocol, which methods will be implemented.
- 
+
  @return A new fake object with implemented protocol methods.
  */
 + (id)fakeObjectWithProtocol:(Protocol *)protocol;
@@ -38,8 +38,8 @@
  Overrides implementation of selected instance method.
 
  @param selector The method selector, which implementation we want to override.
-
  @param block Block which will be executed when overridden method will be called.
+
  @warning It is recommended to call this method only on fake objects
  @warning First block argument will always be fake object's future self, other arguments will be in the same order as in the method.
  */
